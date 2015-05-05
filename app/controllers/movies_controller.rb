7f4f0844f817ad93f16c10ee2a4ddc56de880cb2
Ashley Ongsarte <aongsarte@mills.edu>
2015-05-05 07:32:53 -0300

    Still Working, Always Working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,34 +6,34 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 #HW2 Problem 1.b
-  # def index
-  #   @movies = Movie.order(params[:sort_param])
-  #   @all_ratings = Movie.all_ratings
-  # end
-
   def index
-    if(!params.has_key?(:sort) && !params.has_key?(:ratings))
-      if(session.has_key?(:sort) || session.has_key?(:ratings))
-        redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
-        end
-      end
-      @sort = params.has_key?(:sort) ? (session[:sort] = params[:sort]) : session[:sort]
-      @all_ratings = Movie.all_ratings.keys
-      @ratings = params[:ratings]
-      if(@ratings != nil)
-        ratings = @ratings.keys
-        session[:ratings] = @ratings
-      else
-        if(!params.has_key?(:commit) && !params.has_key?(:sort))
-          ratings = Movie.all_ratings.keys
-          session[:ratings] = Movie.all_ratings
-        else
-          ratings = session[:ratings].keys
-        end
-      end
-      @movies = Movie.order(@sort).find_all_by_rating(ratings)
-      @mark = ratings
+    @movies = Movie.order(params[:sort_param])
+    @all_ratings = Movie.all_ratings
   end
+
+  # def index
+  #   if(!params.has_key?(:sort) && !params.has_key?(:ratings))
+  #     if(session.has_key?(:sort) || session.has_key?(:ratings))
+  #       redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
+  #       end
+  #     end
+  #     @sort = params.has_key?(:sort) ? (session[:sort] = params[:sort]) : session[:sort]
+  #     @all_ratings = Movie.all_ratings.keys
+  #     @ratings = params[:ratings]
+  #     if(@ratings != nil)
+  #       ratings = @ratings.keys
+  #       session[:ratings] = @ratings
+  #     else
+  #       if(!params.has_key?(:commit) && !params.has_key?(:sort))
+  #         ratings = Movie.all_ratings.keys
+  #         session[:ratings] = Movie.all_ratings
+  #       else
+  #         ratings = session[:ratings].keys
+  #       end
+  #     end
+  #     @movies = Movie.order(@sort).find_all_by_rating(ratings)
+  #     @mark = ratings
+  # end
 
 
 
